6ae30693a0efd1d8a1378d2bd28358b72d615341
Paola Abrantes <167098279+paolaabrantes@users.noreply.github.com>
2024-12-09 12:11:50 -0300

    Layout inicial Gênero Narrativo

---
+++ front/src/components/layouts/Narrativo.jsx
@@ -0,0 +1,77 @@
+import '../../css/geralGeneros.css'
+
+const Narrativo = () => {
+  const categorias = {
+    EPOPEIA: [
+      'https://http2.mlstatic.com/D_NQ_NP_721511-MLB20567637142_012016-O.webp',
+      'https://http2.mlstatic.com/D_NQ_NP_721511-MLB20567637142_012016-O.webp',
+      'https://http2.mlstatic.com/D_NQ_NP_721511-MLB20567637142_012016-O.webp',
+      'https://http2.mlstatic.com/D_NQ_NP_721511-MLB20567637142_012016-O.webp',
+      'https://http2.mlstatic.com/D_NQ_NP_721511-MLB20567637142_012016-O.webp',
+      'https://http2.mlstatic.com/D_NQ_NP_721511-MLB20567637142_012016-O.webp',
+      'https://http2.mlstatic.com/D_NQ_NP_721511-MLB20567637142_012016-O.webp'
+    ],
+
+    ROMANCE: [
+      'https://http2.mlstatic.com/D_NQ_NP_721511-MLB20567637142_012016-O.webp',
+      'https://http2.mlstatic.com/D_NQ_NP_721511-MLB20567637142_012016-O.webp',
+      'https://http2.mlstatic.com/D_NQ_NP_721511-MLB20567637142_012016-O.webp',
+      'https://http2.mlstatic.com/D_NQ_NP_721511-MLB20567637142_012016-O.webp',
+      'https://http2.mlstatic.com/D_NQ_NP_721511-MLB20567637142_012016-O.webp',
+      'https://http2.mlstatic.com/D_NQ_NP_721511-MLB20567637142_012016-O.webp',
+      'https://http2.mlstatic.com/D_NQ_NP_721511-MLB20567637142_012016-O.webp'
+    ],
+
+    CONTO: [
+      'https://http2.mlstatic.com/D_NQ_NP_721511-MLB20567637142_012016-O.webp',
+      'https://http2.mlstatic.com/D_NQ_NP_721511-MLB20567637142_012016-O.webp',
+      'https://http2.mlstatic.com/D_NQ_NP_721511-MLB20567637142_012016-O.webp',
+      'https://http2.mlstatic.com/D_NQ_NP_721511-MLB20567637142_012016-O.webp',
+      'https://http2.mlstatic.com/D_NQ_NP_721511-MLB20567637142_012016-O.webp',
+      'https://http2.mlstatic.com/D_NQ_NP_721511-MLB20567637142_012016-O.webp',
+      'https://http2.mlstatic.com/D_NQ_NP_721511-MLB20567637142_012016-O.webp'
+    ],
+
+    NOVELA: [
+      'https://http2.mlstatic.com/D_NQ_NP_721511-MLB20567637142_012016-O.webp',
+      'https://http2.mlstatic.com/D_NQ_NP_721511-MLB20567637142_012016-O.webp',
+      'https://http2.mlstatic.com/D_NQ_NP_721511-MLB20567637142_012016-O.webp',
+      'https://http2.mlstatic.com/D_NQ_NP_721511-MLB20567637142_012016-O.webp',
+      'https://http2.mlstatic.com/D_NQ_NP_721511-MLB20567637142_012016-O.webp',
+      'https://http2.mlstatic.com/D_NQ_NP_721511-MLB20567637142_012016-O.webp',
+      'https://http2.mlstatic.com/D_NQ_NP_721511-MLB20567637142_012016-O.webp'
+    ],
+
+    FÁBULA: [
+      'https://http2.mlstatic.com/D_NQ_NP_721511-MLB20567637142_012016-O.webp',
+      'https://http2.mlstatic.com/D_NQ_NP_721511-MLB20567637142_012016-O.webp',
+      'https://http2.mlstatic.com/D_NQ_NP_721511-MLB20567637142_012016-O.webp',
+      'https://http2.mlstatic.com/D_NQ_NP_721511-MLB20567637142_012016-O.webp',
+      'https://http2.mlstatic.com/D_NQ_NP_721511-MLB20567637142_012016-O.webp',
+      'https://http2.mlstatic.com/D_NQ_NP_721511-MLB20567637142_012016-O.webp',
+      'https://http2.mlstatic.com/D_NQ_NP_721511-MLB20567637142_012016-O.webp'
+    ],
+  };
+
+  return (
+    <div className="container">
+      <h1 className="titulo">NARRATIVO</h1>
+      {Object.entries(categorias).map(([categoria, imagens]) => (
+        <div key={categoria} className="categoria">
+          <h2 className="subtitulo">{categoria}</h2>
+          <div className="livros">
+            {imagens.map((url, index) => (
+              <div
+                key={index}
+                className="card"
+                style={{ backgroundImage: `url(${url})` }}
+              ></div>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Narrativo;
